446a42780f2b93f3691876b9db7e4fbd2aeaa068
harvinder <harkimannat@outlook.com>
2019-08-28 13:56:15 +1000

    Add project files.

---
+++ App5/ViewModels/MasterDetailViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using App5.Core.Models;
+using App5.Core.Services;
+using App5.Helpers;
+
+using Microsoft.Toolkit.Uwp.UI.Controls;
+
+namespace App5.ViewModels
+{
+    public class MasterDetailViewModel : Observable
+    {
+        private SampleOrder _selected;
+
+        public SampleOrder Selected
+        {
+            get { return _selected; }
+            set { Set(ref _selected, value); }
+        }
+
+        public ObservableCollection<SampleOrder> SampleItems { get; private set; } = new ObservableCollection<SampleOrder>();
+
+        public MasterDetailViewModel()
+        {
+        }
+
+        public async Task LoadDataAsync(MasterDetailsViewState viewState)
+        {
+            SampleItems.Clear();
+
+            var data = await SampleDataService.GetMasterDetailDataAsync();
+
+            foreach (var item in data)
+            {
+                SampleItems.Add(item);
+            }
+
+            if (viewState == MasterDetailsViewState.Both)
+            {
+                Selected = SampleItems.First();
+            }
+        }
+    }
+}
---
+++ App5/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using App5.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace App5.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
